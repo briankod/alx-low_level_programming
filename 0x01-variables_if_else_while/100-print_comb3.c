ccd6da38dfa1a8a034beb393d199c67e5979ea1a
briankod <brianmugambi199@gmail.com>
2022-03-11 08:45:34 -0800

    My first C - Variables,if,else,while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, d;
+	for(i = '0'; i < '9'; ++i)
+	{
+		for(d = i + 1; d <= '9'; ++d)
+		{
+			if (d != i)
+			{
+				putchar(i);
+				putchar(d);
+				if(i == '8' && d == '9')
+					comtinue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
