d23409d61fcca4847811e8444f5b5ff7bfaaab59
briankod <brianmugambi199@gmail.com>
2022-03-15 12:13:05 -0700

    My first C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,16 @@
 
 /**
  * main - check the code.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	char name[] = "_putchar";
 	int i;
-	
+
 	for (i = 0; i < 9; ++i)
 		_putchar(name[i]);
 	_putchar('\n');
-	return (0);
+	return 0;
 }
