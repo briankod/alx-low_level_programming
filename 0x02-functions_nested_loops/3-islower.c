c08b4ec3ecbe64e9f3f156fe027b9058636de865
briankod <brianmugambi199@gmail.com>
2022-03-15 22:40:35 -0700

    My first C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,12 +7,12 @@
  */
 int _islower(int c)
 {
-	char c;
+	char ch;
 	int i = 0;
 
-	for (c = 'a'; c <= 'z'; ++c)
+	for (ch = 'a'; ch <= 'z'; ++ch)
 	{
-		if (c == c)
+		if (ch == ch)
 			i = 1;
 	}
 	return (i);
