645426b60789034e326ae9cd4f343a68c97bffbd
briankod <brianmugambi199@gmail.com>
2022-03-16 06:39:47 -0700

    My first C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,49 +7,36 @@
  */
 void print_times_table(int n)
 {
-	int i, d;
-
-	if (n > 0 && n < 15)
+	int i, d, r;
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= n; ++i)
 		{
 			_putchar('0');
-			for (d = 1; d <= n; d++)
-				putformat(i * d);
+
+			for (d = 1; d <= n; ++d)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				r = i * d;
+
+				if (r <= 99)
+					_putchar(' ');
+				if (r <= 9)
+					_putchar(' ');
+				if (r >= 100)
+				{
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10)) % 10 + '0');
+				}
+				else if (r <= 99 && r >= 10)
+				{
+					_putchar((r / 10) + '0');
+				}
+				_putchar((r % 10) + '0');
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - check the code.
- * @n: an input integer.
- * Return: nothing.
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
