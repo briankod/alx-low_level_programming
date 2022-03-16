c90b4c32a47a6492753055508edcb8bc057417e9
briankod <brianmugambi199@gmail.com>
2022-03-16 05:46:05 -0700

    My first C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 
 	if (n > 0 && n < 15)
 	{
-		for (i = 0; i <= n; ++i)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (d = 1; d <= n; ++d)
+			for (d = 1; d <= n; d++)
 				putformat(i * d);
 			_putchar('\n');
 		}
