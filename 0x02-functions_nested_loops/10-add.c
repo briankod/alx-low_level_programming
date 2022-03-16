4d408626bdaecdc9187707e4769184302504908c
briankod <brianmugambi199@gmail.com>
2022-03-16 04:09:34 -0700

    My first C - Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - check the code.
- *
+ * @a - an input integer.
+ * @b - an input integer.
  * Return: sum.
  */
 int add(int a, int b)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; ++n)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; --n)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
