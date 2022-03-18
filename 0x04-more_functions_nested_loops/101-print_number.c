e47b37872227807f227c6a1e1d916fff2eff4b4c
briankod <brianmugambi199@gmail.com>
2022-03-17 17:21:06 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,11 +13,8 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n == 0)
-		putchar(0);
+	if (n / 10)
+		print_number(n / 10);
 
-	if (n/10)
-		print_number(n/10);
-
-	_putchar( n%10 + '0');
+	_putchar( n % 10 + '0');
 }
