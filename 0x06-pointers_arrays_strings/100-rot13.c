d5fbfff0a95226c0620df1721b7df8e3b6ab48a5
briankod <99045621+briankod@users.noreply.github.com>
2022-03-24 16:51:10 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,29 @@
-#include "main,h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * rot13 - check the code.
- * @s: an input string to encode using  rot13.
- * Return: an encode string.
+ * rot13 - function that encodes a string into 1337
+ * @s: string to be encoded using rot13
+ * Return: the encoded string
  */
+
 char *rot13(char *s)
 {
 	int i = 0, j;
-	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (*(s + i) != '\0')
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (*(s + i) == c[j])
+			if (*(s + i) == alpha[j])
 			{
-				*(s + i) =rot13[j];
+				*(s + i) = rot13[j];
 				break;
 			}
 		}
 		i++;
 	}
-
 	return (s);
 }
