de33b0c8964fad3201e3aac75531f89a7234c342
briankod <99045621+briankod@users.noreply.github.com>
2022-03-24 17:17:22 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,45 +1,20 @@
 #include "main.h"
 
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
+ * print_number - check the code.
+ * @n: an input integer.
  * Return: nothing.
  */
 void print_number(int n)
 {
-	int m, c, num; 
-
-	num = n;
-	
-	if (num < 0)
+	if (n < 0)
 	{
-		num *= -1;
 		_putchar('-');
+		n = -n;
 	}
-	
-	
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-	
-	
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar( n % 10 + '0');
 }
