0f5edcd6a9423f86384b4feb34072876c974d111
briankod <99045621+briankod@users.noreply.github.com>
2022-03-28 15:49:55 +0300

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,14 +3,14 @@
 /**
  * _memset - a function that fills memory with a constant byte.
  * @s: starting address of the memory to be filled.
- * @b: value to be filled.
+ * @b: char value to be filled.
  * @n: number of bytes to filled.
- * Return: a pointer to the memory area s.
+ * Return: returns pointer to location filled
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
-	
+	char **ptr = s;
+
 	while (n--)
 	{
 		*s = b;
