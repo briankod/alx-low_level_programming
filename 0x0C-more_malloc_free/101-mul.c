9f6cc0250f0b7f55053e44018afa8887b9437cf0
briankod <brianmugambi199@gmail.com>
2022-04-07 11:37:18 -0700

    More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdio.lib>
 
+/**
+ * main - check the code.
+ * @argc: number of arguments.
+ * @argv: arguments.
+ * Return nothing.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n")
+			exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		if (num1 > 0 && num2 > 0)
+		{
+			result = num1 * num2;
+			printf("%d\n", result);
+		}
+		return (0);
+	}
+}
