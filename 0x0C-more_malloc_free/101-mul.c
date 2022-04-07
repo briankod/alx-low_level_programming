917c5a4052e7fd789065b796e0824be5a7eb5a72
briankod <brianmugambi199@gmail.com>
2022-04-07 11:52:04 -0700

    More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,11 +20,8 @@ int main(int argc, char **argv)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-		if (num1 > 0 && num2 > 0)
-		{
-			result = num1 * num2;
-			printf("%d\n", result);
-		}
+		result = num1 * num2;
+		printf("%d\n", result);
 		return (0);
 	}
 }
