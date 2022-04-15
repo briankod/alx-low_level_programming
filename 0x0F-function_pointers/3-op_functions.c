b33689c5c7be62e3993862df55f8546e3900b9d3
briankod <brianmugambi199@gmail.com>
2022-04-15 02:48:37 -0700

    My first C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - a function that returns the sum of a and b.
+ * @a: an input integer.
+ * @b: an input integer.
+ * Return: The result to sum a + b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a funtion that returns the subtract of a and b.
+ * @a: an input integer.
+ * @b: an input integer.
+ * Return: The result to subtract a - b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that returns the multiply of a and b.
+ * @a: an input integer.
+ * @b: an input integer.
+ * Return: The result to multiply a * b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that retukrns the divide of a and b.
+ * @a: an input integer.
+ * @b: an input integer.
+ * Return: The result to divide a / b.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that returns the module of a and b.
+ * @a: an input integer.
+ * @b: an input integer.
+ * Return: The result to module a % b.
+ */
+int op_mob(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
