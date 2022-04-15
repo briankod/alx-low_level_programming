1fe33055836bda05311758daac0afb9ca4bd4814
briankod <brianmugambi199@gmail.com>
2022-04-15 07:36:21 -0700

    My first C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,23 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, c;
-	int (*f)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-	if (f == NULL)
+
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	c = f(num1, num2);
-	printf("%d\n", c);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
 	return (0);
 }
