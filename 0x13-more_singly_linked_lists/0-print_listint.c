e4a952544df448dd5e72678d460384a18360016f
briankod <brianmugambi199@gmail.com>
2022-04-27 00:42:51 +0300

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 
 /**
- * print_listint - A function that prints all the elements in the list
- * @h: A pointer to listint structure
- * Return: the number of nodes.
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: pointer to the head of linked list.
+ *
+ * Format: see example.
+ * You are allowed to use printf.
+ *
+ * Return: all the elements of the list.
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
+	/* while we have a pointer to head */
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
+		count++;
 	}
-	return (nodes);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,23 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
+ * insert_nodeint_at_index - function that inserts a new node at,
+ * a given position.
+ * @head: pointer to pointer to the head of linked list.
+ * @idx: index of the list where the new node should be added.
+ * @n: value of the new node.
+ *
+ * if it is not possible to add the new node at index idx, do not,
+ * add the new node and return NULL.
+ *
+ * Return:  the address of the new node, or NULL if it failed.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
 	listint_t *new_node, *temp;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
@@ -19,21 +26,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
 	temp = *head;
 	while (i != idx - 1 && temp != NULL)
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	if (i == idx - 1 && temp != NULL)
 	{
 		new_node->next = temp->next;
