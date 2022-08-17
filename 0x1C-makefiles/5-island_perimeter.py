3362994babb093e7a167d89530cce9bcd3c7750d
briankod <brianmugambi199@gmail.com>
2022-08-17 22:38:16 +0300

    C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,9 +15,9 @@ def island_perimeter(grid):
         for j in range(col_num):
             if grid[i][j] == 1:
                 island_cell += 1
-            if i + 1 < row_num and grid[i + 1][j] == 1:
-                neighbour_cell += 1
-            if j + 1 < col_num and grid[i][j + 1] == 1:
-                neighbour_cell += 1
+                if i + 1 < row_num and grid[i + 1][j] == 1:
+                    neighbour_cell += 1
+                if j + 1 < col_num and grid[i][j + 1] == 1:
+                    neighbour_cell += 1
 
     return island_cell * 4 - neighbour_cell * 2
